d2f8416333f5b337388d2bc7f8d82cb4f7b37e2d
AnjosHD-Black <anjos.schwarz@gmail.com>
2025-11-25 22:16:57 +0100

    Sslv2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // Wichtig: Hierdurch wird der Konfigurator-Code in src/App.jsx gefunden
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
